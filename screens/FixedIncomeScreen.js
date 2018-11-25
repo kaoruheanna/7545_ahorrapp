abd37cf0ea931abeb130852c1fa21baea5ffdcc2
Kaoru Heanna <kaoru.heanna@trocafone.com>
2018-11-25 17:05:29 -0300

    formulario para ingreso fijo

--- screens/FixedIncomeScreen.js
+++ screens/FixedIncomeScreen.js
@@ -1,6 +1,53 @@
 import React from 'react';
 import {Button, Text, View} from 'react-native';
-import {NavigationActions, StackActions} from "react-navigation";
+import t from 'tcomb-form-native';
+import moment from 'moment';
+
+const Form = t.form.Form;
+
+const Category = t.enums({
+    'Sueldo': 'Sueldo',
+    'Honorarios': 'Honorarios',
+    'Otros': 'Otros'
+}, 'Category');
+
+var options = {
+    auto: 'placeholders',
+    fields: {
+        money: {
+            label: 'Monto'
+        },
+        category: {
+            label: 'Categoría',
+            nullOption: false
+        },
+        concept: {
+            label: 'Concepto'
+        },
+        since: {
+            label: 'Desde',
+            mode: 'date',
+            config: {
+                format: (date) => moment(date).format('DD-MM'),
+            },
+        },
+        until: {
+            label: 'Hasta',
+            mode: 'date',
+            config: {
+                format: (date) => moment(date).format('DD-MM'),
+            },
+        }
+    }
+};
+
+const FixedIncome = t.struct({
+    money: t.Number,
+    category: Category,
+    concept: t.maybe(t.String),
+    since: t.Date,
+    until: t.Date,
+});
 
 export default class FixedIncomeScreen extends React.Component {
 
@@ -8,22 +55,39 @@ export default class FixedIncomeScreen extends React.Component {
         title: 'Ingreso Fijo',
     };
 
+    state = {
+        value: {
+            money: null,
+            category: 'Sueldo',
+            concept: '',
+        }
+    };
+
+    onChange = (value) => {
+        this.state.value = value;
+    };
+
+    onPress = () => {
+        var value = this.refs.form.getValue();
+        if (value) {
+            console.log("value: ",this.state);
+        }
+    };
+
     render() {
         return (
             <View>
                 <Text> Esta es la pantalla de ingresos fijos </Text>
 
-                <Button
-                    title="Go to Home"
-                    onPress={() => {
-                        this.props.navigation.dispatch(StackActions.reset({
-                            index: 0,
-                            actions: [
-                                NavigationActions.navigate({ routeName: 'Home' })
-                            ],
-                        }))
-                    }}
+                <Form
+                    ref="form"
+                    type={FixedIncome}
+                    value={this.state.value}
+                    onChange={this.onChange}
+                    options={options}
                 />
+
+                <Button title="Guardar" onPress={this.onPress}/>
             </View>
         );
     }
